73bbbbbda689efeb4af2287340a001508b9d4b2c
dusanzeliar <dusan.zeliar@gmail.com>
2019-11-16 09:09:43 +0100

    skeleton

---
+++ game-of-life/src/test/java/com/solarwinds/AppTest.java
@@ -0,0 +1,22 @@
+package com.solarwinds;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue(true);
+    }
+}
